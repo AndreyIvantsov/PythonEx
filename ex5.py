6d14df5ce0c1470133c60e69346ce034ece6736d
AndreyIvantsov <tetranod@mail.ru>
2019-05-02 00:21:10 +0300

    Добавлен файл ex5.py

---
+++ ex5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+my_name = 'Андрей'
+my_age = 47         # лет
+my_height = 178     # см
+my_weight = 76      # кг
+my_eyes = 'Карие'
+my_teeth = 'Белые'
+my_hair = 'Русые'
+
+print('Давайте погворим о человеке по имени %s' %my_name)
+print('Его рост составляет %d см.' %my_height)
+print('Он весит %d кг.' %my_weight)
+print('На самом деле это не так много.')
+print('У него глаза %s и волосы %s.' %(my_eyes, my_hair))
+print('Его зубы обычно %s, хотя он любит пить кофе.' %my_teeth)
+
+# эта строка довольно сложная, не ошибитесь!
+
+print('Если я сложу %d, %d и %d, то получу %d' %(my_age, my_height, my_weight, my_age + my_height + my_weight))
